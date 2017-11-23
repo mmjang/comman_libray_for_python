cc4bc458d60a1d727ee84d46bd3c7343103d794e
mmjang <752515918@qq.com>
2017-11-23 21:24:38 +0800

    add function for plotting directory

--- common.py
+++ common.py
@@ -1,3 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import re
+
 def plot_time_2d(fname, rmbk = True, save_fig = False):
     '''
     绘制时域系统dat数据二维图
@@ -9,7 +14,6 @@ def plot_time_2d(fname, rmbk = True, save_fig = False):
     f = open(fname,'rb')
     arr = np.fromfile(f, dtype=np.int16)
     arr = arr.reshape(int(arr.shape[0]/ (frame_size/2)),int(frame_size /2))
-    %matplotlib notebook
     signal = arr[:,5:5+640]
     mean = np.mean(signal.T,axis=1)
     if rmbk:
@@ -20,3 +24,14 @@ def plot_time_2d(fname, rmbk = True, save_fig = False):
         plt.savefig(fname + '.png')
     else:
         plt.show()
+
+def plot_all_in_directory(directory, rmbk = True):
+    '''
+    对于时域系统dat数据文件，绘制并保存 directory 文件夹下的所有 dat 文件，默认去直达波
+    directory - 文件夹路径
+    rmbk - 是否去直达波
+    '''
+    for fname in [f for f in os.listdir(directory) if re.match(r'[0-9]+\.dat', f)]:
+        plot_time_2d(fname, rmbk = rmbk, save_fig = True)
+        print('{0} 已绘制并保存'.format(fname))
+
