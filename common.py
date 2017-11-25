3af426eac3ef1ef12e4b75b2381ddf0868ad1a3c
mmjang <752515918@qq.com>
2017-11-25 14:22:00 +0800

    add readdata and rmbk

--- common.py
+++ common.py
@@ -1,7 +1,31 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import os
 import re
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def read_dat(fname, frame_size = 1326, number_of_points = 640, start_of_signal = 5):
+    '''
+    读取时域系统 dat 数据
+    用法：
+        read_dat('test.dat')
+    参数：
+        fname - 文件名
+        frame_size - 帧长度
+        number_of_points - 信号点数
+        start_of_signal - 信号开始位置
+    '''
+    f = open(fname, 'rb')
+    arr = np.fromfile(f, dtype = np.int16)
+    arr = arr.reshape(int(arr.shape[0] / (frame_size / 2)), int(frame_size / 2))
+    signal = arr[:, start_of_signal : start_of_signal + number_of_points]
+    return signal
+
+def remove_background(data):
+
+    mean = np.mean(data.T, axis = 1)
+    return data - mean
 
 def plot_time_2d(ax, fname, rmbk = True):
     '''
@@ -15,16 +39,11 @@ def plot_time_2d(ax, fname, rmbk = True):
     rmbk - 是否去背景
     save_fig - 是否保存图片
     '''
-    frame_size = 1326
-    f = open(fname,'rb')
-    arr = np.fromfile(f, dtype=np.int16)
-    arr = arr.reshape(int(arr.shape[0]/ (frame_size/2)),int(frame_size /2))
-    signal = arr[:,5:5+640]
-    mean = np.mean(signal.T,axis=1)
+    signal = read_dat(fname)
     if rmbk:
-        out = ax.imshow(arr[:,5:5+ 640] - mean)
+        out = ax.imshow(remove_background(signal))
     else:
-        out = ax.imshow(arr[:,5:5+ 640])
+        out = ax.imshow(signal)
     return out
 
 def plot_all_in_directory(directory, rmbk = True):
@@ -37,9 +56,14 @@ def plot_all_in_directory(directory, rmbk = True):
     for fname in [f for f in os.listdir(directory) if re.match(r'[0-9]+\.dat$', f)]:
         try:
             fig, ax = plt.subplots(1,1)
-            plot_time_2d(ax, fname, rmbk = rmbk)
+            im = plot_time_2d(ax, fname, rmbk = rmbk)
+            plot.colorbar(im)
             fig.savefig(fname + '.png')
             print('{0} 已绘制并保存'.format(fname))
         except Exception:
             print('{0} 出错'.format(fname))
 
+if __name__ == '__main__':
+
+    name = 'D:\\1123\\20171123174059.dat'
+    print(read_dat(name).shape)
